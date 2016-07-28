18ff9fd5b2a4ef41cbc4b02e783c4fb5422fb73f
totuta <totuta@gmail.com>
2016-07-28 19:04:21 -0400

    update

--- twitter-gathering.py
+++ twitter-gathering.py
@@ -5,39 +5,45 @@
 # description: storing tweets about any keywords
 # author: Wonchang Chung
 
-import tweepy
+import tweepy   # pip install tweepy : you might need virtualenv to use this package
 import json
+
 
 #################
 # SEARCH OPTIONS
 #################
 
 # maximum number of tweets per searching
+#   maximum rate for the Search API is 2,000tweets/15minutes
 MAX_TWEETS = 2000
 
 # set output file name
-OUT_FILE = '/proj/nlp/users/wc2550/tweets_orlando_1.json'	# path was set to use the big storing space
-# OUT_FILE = 'tweets_stress_id_test.json'
+OUT_FILE = 'DIRECTORY/FILENAME'
+# OUT_FILE = 'output/tweets.json'							# example
 
 # set search query
-QUERY = '"orlando" OR "Orlando"'
+#   refer to https://dev.twitter.com/rest/public/search 
+QUERY = 'YOUR_QUERY_HERE'
+# QUERY = '"Columbia University" OR "columbia university"'  # example 1
+# QUERY = '"#DeepLearning" OR "#MachineLearning"'           # example 2
+
 
 #################
 # AUTHENTICATION
 #################
 
-# authentication information of 'totuta'
-CONSUMER_KEY = 'muatqgdF1LWLI3ou9PmKo2Hwe'
-CONSUMER_SECRET = '9doWWrSm2U5wx0QMFoGlFQs7LQeCwBI9GsXlp2pPvihyDcFvtp'
-ACCESS_KEY = '54503673-58jmZaHVCVbYQYy1bffg1hhou9PtQMWSWKsBRW2J1'
-ACCESS_SECRET = 'Dlae25FP952xarSCdOY8qa6RIeMYsKjTuDG920LXtymHe'
+# your authentication information
+#   to obtain authentication info, go to https://apps.twitter.com/app/new
+CONSUMER_KEY = 'YOUR_COSUMER_KEY_HERE'
+CONSUMER_SECRET = 'YOUR_CONSUMER_SECRET_HERE'
+ACCESS_KEY = 'YOUR_ACCESS_KEY_HERE'
+ACCESS_SECRET = 'YOUR_ACCESS_SECRET_HERE'
 
 # authentication process
 auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
 auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
 api = tweepy.API(auth)
 
-#################
 
 def main():
 
@@ -53,6 +59,7 @@ def main():
 	# storing in json format
 	outFile = open(OUT_FILE,'a')
 
+	# for a full map of tweet data, refer to http://mike.teczno.com/img/raffi-krikorian-map-of-a-tweet.png
 	for i in range(0,len(searched_tweets)):
 
 		json_temp = {}
@@ -60,21 +67,21 @@ def main():
 		json_temp['id'] = searched_tweets[i].id
 		json_temp['text'] = searched_tweets[i].text.encode("utf-8")
 		json_temp['created_at'] = str(searched_tweets[i].created_at)
-		# json_temp['in_reply_to_user_id'] = searched_tweets[i].in_reply_to_user_id
-		# json_temp['geo'] = searched_tweets[i].geo
-		# json_temp['source'] = searched_tweets[i].source.encode("utf-8")
+		json_temp['in_reply_to_user_id'] = searched_tweets[i].in_reply_to_user_id
+		json_temp['geo'] = searched_tweets[i].geo
+		json_temp['source'] = searched_tweets[i].source.encode("utf-8")
 
 		json_temp['user'] = {}
 		json_temp['user']['id'] = searched_tweets[i].user.id
-		# json_temp['user']['description'] = searched_tweets[i].user.description.encode("utf-8")
+		json_temp['user']['description'] = searched_tweets[i].user.description.encode("utf-8")
 		json_temp['user']['location'] = searched_tweets[i].user.location.encode("utf-8")
-		# json_temp['user']['created_at'] = str(searched_tweets[i].user.created_at)
-		# json_temp['user']['favourites_count'] = searched_tweets[i].user.favourites_count
-		# json_temp['user']['friends_count'] = searched_tweets[i].user.friends_count
-		# json_temp['user']['time_zone'] = searched_tweets[i].user.time_zone
+		json_temp['user']['created_at'] = str(searched_tweets[i].user.created_at)
+		json_temp['user']['favourites_count'] = searched_tweets[i].user.favourites_count
+		json_temp['user']['friends_count'] = searched_tweets[i].user.friends_count
+		json_temp['user']['time_zone'] = searched_tweets[i].user.time_zone
 		json_temp['user']['utc_offset'] = searched_tweets[i].user.utc_offset
 		json_temp['user']['followers_count'] = searched_tweets[i].user.followers_count
-		# json_temp['user']['geo_enabled'] = searched_tweets[i].user.geo_enabled
+		json_temp['user']['geo_enabled'] = searched_tweets[i].user.geo_enabled
 
 		outFile.write(json.dumps(json_temp, indent = 4))
 		outFile.write(',\n')
